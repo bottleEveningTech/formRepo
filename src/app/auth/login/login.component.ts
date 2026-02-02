66b27e0f8c977a00f430973907b03b9e8c4d2a0c
bottleEveningTech <tanishkasaxena2408@gmail.com>
2026-02-02 19:40:58 +0530

    Managing template driven forms

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [FormsModule],
+  
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+
+  onSubmit(formData: NgForm){
+    console.log(formData);
+    const email = formData.value.emailName;
+    const password = formData.value.passwordName;
+    console.log('Email:', email);
+    console.log('Password:', password);
+  }
+}
